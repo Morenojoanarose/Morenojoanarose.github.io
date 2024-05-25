82166009b3e7b935e562aa249c0ac61dd9fb5466
Morenojoanarose <morenojoanarose06@gmail.com>
2024-05-25 06:01:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const btnLike1 = document.getElementById("btnLike1");
-const count1 = document.getElementById("count1");
+const btnLike1 = document.getElementById("btnLike1")
+const countLike1 = document.getElementById("countLike1")
 
-function countLike1() {
-  let likeCount = parseInt(count1.value) + 1;
-  count1.textContent = likeCount.toString();
+function clickLike1(){
+  let totalLikes = parseInt(countLike1.value) + 1
+  countLike1.textContent = totalLikes.toString()
 }
+btnLike1.addEventListener("click",clickLike1)
 
-btnLike1.addEventListener("click", countLike1);
+function clickLike2(){
+  let totalLikes = parseInt(countLike2.value) + 1
+  countLike2.textContent = totalLikes.toString()
+}
+btnLike2.addEventListener("click",clickLike2
