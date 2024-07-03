59ee74242c1245b05f88fd019b6551a4d705925a
Morenojoanarose <morenojoanarose06@gmail.com>
2024-07-03 19:42:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,3 @@
-const btnLike1 = document.getElementById("btnLike1");
-const count1 = document.getElementById("count1");
-
-function countLike1() {
-  let likeCounts = parseInt(count1.value) + 1;
-  count1.textContent = likeCounts.toString();
-}
-
-const btnLike2 = document.getElementById("btnLike2");
-const count2 = document.getElementById("count2"); 
-
-function countLike2() {
-  let likeCounts = parseInt(count2.value) + 1;
-  count2.textContent = likeCounts.toString();
-}
-
-const btnDLike1 = document.getElementById("btnDLike1");
-const countD1 = document.getElementById("countD1"); 
-
-function countDLike1() {
-  let likeCounts = parseInt(countD1.value) + 1;
-  countD1.textContent = likeCounts.toString();
-}
-
-const btnDLike2 = document.getElementById("btnDLike2");
-const countD2 = document.getElementById("countD2"); 
-
-function countDLike2() {
-  let likeCounts = parseInt(countD2.value) + 1;
-  countD2.textContent = likeCounts.toString();
-}
-
-
-
-btnLike1.addEventListener("click", countLike1);
-btnLike2.addEventListener("click", countLike2);
-btnDLike1.addEventListener("click", countDLike1);
-btnDLike2.addEventListener("click", countDLike2);
-
-const submit = document.getElementById("submit") 
-const comment = document.getElementById("comment")
-const commentbox = document.getElementById("commentbox")
-
-function submitComment(){
-    commentbox.textContent += comment.value.toString() + "\n"
-}
-submit.addEventListener("click",submitComment)
-
 var product1 = document.getElementById("product1");
 var qty1 = document.getElementById("qty1");
 var price1 = document.getElementById("price1");
@@ -135,7 +87,7 @@ function finishOrder() {
     var cashAmount = parseFloat(cash.value);
 
     if (cashAmount >= totalAmount && totalAmount > 0) {
-        alert("Order Successful!\n\n" + "Products Purchased:\n" + carts.textContent + "\nTHANK YOU !");
+        alert("Order Successful!\n\n" + "Products Purchased:\n" + carts.textContent + "\n HAPPY SHOPPING !");
         carts.textContent = "";
         total.value = "";
         cash.value = "";
